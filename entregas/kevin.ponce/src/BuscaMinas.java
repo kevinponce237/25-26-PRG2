ed763b7e25e28d01720172f154fae5d4c52213fc
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-02-17 13:07:12 +0100

    estructura incial

---
+++ entregas/kevin.ponce/src/BuscaMinas.java
@@ -0,0 +1,48 @@
+public class Buscaminas {
+    static int[] listaMinasX;
+    static int[] listaMinasY;
+
+    static int numMinasActivas;
+    static boolean continuarJuego = true;
+
+    static final int minasAGenerar = 5;
+    static final int minasParaPerder = 3;
+    static final int numColumnasDelTablero = 7;
+    static final int numFilasDelTablero = 5;
+    static int numeroDePosicionesRevisadas = 0;
+    static char[][] tablero;
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static char[][] generarTablero() {
+        return null;
+    }
+
+    public static void generarIndicesAleatorios() {
+
+    }
+
+    public static void mostrarTableroYMenu() {
+
+    }
+
+    public static boolean verificar(int posicionX, int posicionY) {
+        return false;
+    }
+
+    public static boolean verificarEstado() {
+        return false;
+    }
+
+    public static boolean estaEnLista(int valor, int[] lista) {
+        return false;
+    }
+
+    public static int calcularPosicionesSeguras() {
+        return 0;
+    }
+
+}
