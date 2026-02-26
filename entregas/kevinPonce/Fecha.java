299c606a34a473746661971ffba5d650b4485f2b
kevin@ponce <kevin.ponce@alumnos.uneatlantico.es>
2026-02-26 21:33:46 +0100

    vista publica clase fecha

---
+++ entregas/kevinPonce/Fecha.java
@@ -0,0 +1,58 @@
+class fecha {
+    private int dia;
+    private int mes;
+    private int año;
+
+    public fecha(int dia, int mes, int año) {
+ 
+    }
+
+    public fecha() {
+
+    }
+
+    public int getDia() {
+ 
+    }
+
+    public void aumentarDias(int numeroDias) {
+     
+    }
+
+    public void aumentarMeses(int numeroMeses) {
+    
+    }
+
+    public void aumentarAños(int numeroAños) {
+  
+    }
+
+    public void mostrar() {
+    }
+
+    public int getMes() {
+        
+    }
+
+    public int getAño() {
+        
+    }
+
+    public void editarFechaCompleta(int dia, int mes, int año) {
+        
+    }
+
+    public boolean esBisiesto() {
+        
+    }
+
+    public boolean esMayorQue(fecha fecha) {
+        
+    }
+
+    public int diasFaltantes() {
+        
+    }
+
+
+}
