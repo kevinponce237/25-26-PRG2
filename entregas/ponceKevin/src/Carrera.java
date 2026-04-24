99fdc3105ae9829672929d0419a68d32b8bc3eee
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-04-24 09:34:35 +0000

    feat: mejorar la lógica de la carrera añadiendo limpieza de consola y retraso en la ejecución

--- entregas/ponceKevin/src/Carrera.java
+++ entregas/ponceKevin/src/Carrera.java
@@ -1,11 +1,14 @@
 public class Carrera {
     boolean carreraEnCurso = false;
+
     public void jugar() {
         carreraEnCurso = true;
         Caballo caballo1 = new Caballo("Relámpago");
         Caballo caballo2 = new Caballo("Tornado");
         Caballo caballo3 = new Caballo("Centella");
         final int tamañoPista = 40;
+        final int delayMs = 400;
+
         System.out.println("Iniciando la carrera...");
        
         Pista pista = new Pista(tamañoPista);
@@ -14,9 +17,26 @@ public class Carrera {
         pista.agregarCaballo(caballo3);
         while (carreraEnCurso) {
            this.carreraEnCurso = !pista.hayGanador();
+           limpiarConsola();
            pista.mostrarPosiciones();
+           esperar(delayMs);
            pista.avanzarCaballos();
         }
+        limpiarConsola();
+        pista.mostrarPosiciones();
         pista.mostrarGanadores();
+    }
+
+    private void limpiarConsola() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private void esperar(int milisegundos) {
+        try {
+            Thread.sleep(milisegundos);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
