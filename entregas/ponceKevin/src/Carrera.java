a9e48d1c73a06b2075b39129c4bf90848a534d59
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-04-24 09:32:05 +0000

    feat: agregar tercer caballo a la carrera para mejorar la simulación

--- entregas/ponceKevin/src/Carrera.java
+++ entregas/ponceKevin/src/Carrera.java
@@ -4,12 +4,14 @@ public class Carrera {
         carreraEnCurso = true;
         Caballo caballo1 = new Caballo("Relámpago");
         Caballo caballo2 = new Caballo("Tornado");
+        Caballo caballo3 = new Caballo("Centella");
         final int tamañoPista = 40;
         System.out.println("Iniciando la carrera...");
        
         Pista pista = new Pista(tamañoPista);
         pista.agregarCaballo(caballo1);
         pista.agregarCaballo(caballo2);
+        pista.agregarCaballo(caballo3);
         while (carreraEnCurso) {
            this.carreraEnCurso = !pista.hayGanador();
            pista.mostrarPosiciones();
