7d98af1adf5db6c9897d412e4e972825c5f8923f
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-04-24 09:44:39 +0000

    refactor: reorganizar métodos en la clase Carrera para mejorar la legibilidad y la estructura del código

--- entregas/ponceKevin/src/Carrera.java
+++ entregas/ponceKevin/src/Carrera.java
@@ -1,32 +1,29 @@
 public class Carrera {
-    boolean carreraEnCurso = false;
-
+    private boolean carreraEnCurso = false;
+    private Pista pista;
+    final int tamañoPista = 40;
+    final int delayMs = 400;
     public void jugar() {
-        carreraEnCurso = true;
-        Caballo caballo1 = new Caballo("Relámpago");
-        Caballo caballo2 = new Caballo("Tornado");
-        Caballo caballo3 = new Caballo("Centella");
-        final int tamañoPista = 40;
-        final int delayMs = 400;
-
-        System.out.println("Iniciando la carrera...");
-       
-        Pista pista = new Pista(tamañoPista);
-        pista.agregarCaballo(caballo1);
-        pista.agregarCaballo(caballo2);
-        pista.agregarCaballo(caballo3);
+        this.incializarCarrera();
         while (carreraEnCurso) {
-           this.carreraEnCurso = !pista.hayGanador();
-           limpiarConsola();
-           pista.mostrarPosiciones();
-           esperar(delayMs);
-           pista.avanzarCaballos();
+          this.avanzarCarrera();
         }
         limpiarConsola();
         pista.mostrarPosiciones();
         pista.mostrarGanadores();
     }
 
+    public void incializarCarrera() {
+        this.carreraEnCurso = true;
+        Caballo caballo1 = new Caballo("Relámpago");
+        Caballo caballo2 = new Caballo("Tornado");
+        Caballo caballo3 = new Caballo("Centella");
+        pista = new Pista(tamañoPista);
+        pista.agregarCaballo(caballo1);
+        pista.agregarCaballo(caballo2);
+        pista.agregarCaballo(caballo3);
+
+    }
     private void limpiarConsola() {
         for (int i = 0; i < 40; i++) {
             System.out.println();
@@ -40,4 +37,13 @@ public class Carrera {
             Thread.currentThread().interrupt();
         }
     }
+
+    public  void avanzarCarrera() {
+        carreraEnCurso = !pista.hayGanador();
+        limpiarConsola();
+        pista.mostrarPosiciones();
+        esperar(delayMs);
+        pista.avanzarCaballos();
+    }
+
 }
